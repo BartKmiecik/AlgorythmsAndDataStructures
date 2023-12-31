30d57f304a658f5a287173efae8ab875d6232fc5
Bart <kmiecikjob@gmail.com>
2023-12-31 21:25:33 +0100

    Partial sum and partial square

---
+++ Algorythms/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum2.py
@@ -0,0 +1,24 @@
+import time
+
+
+def fibonacci_partial_sum_naive(from_, to):
+    start = time.time()
+
+    _sum = 0
+
+    current = 0
+    _next  = 1
+
+    for i in range(to + 1):
+        if i >= from_:
+            _sum += current
+
+        current, _next = _next % 10, (current + _next) % 10
+    end = time.time()
+    print(end-start)
+    return _sum % 10
+
+
+if __name__ == '__main__':
+    from_, to = map(int, input().split())
+    print(fibonacci_partial_sum_naive(from_, to))

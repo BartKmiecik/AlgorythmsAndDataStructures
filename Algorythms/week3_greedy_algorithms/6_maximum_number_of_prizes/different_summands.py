1e7403e403e8b482f812693cf4a3c755d8431c52
Bart <kmiecikjob@gmail.com>
2024-01-01 14:41:17 +0100

    Different summands

--- Algorythms/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ Algorythms/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,5 +1,24 @@
 def optimal_summands(n):
     summands = []
+    if n <= 2:
+        summands.append(n)
+        return summands
+    if n == 3:
+        summands.append(1)
+        summands.append(2)
+        return summands
+    next = 1
+    modulo = next + next+1
+    total_diff = next
+    while n - total_diff >= modulo:
+        # print(f'Diff: {n - total_diff} and next: {next}, modulo{modulo}')
+        summands.append(next)
+        next += 1
+        total_diff += next
+        # print(f'Diff: {n - total_diff} and next: {next}, modulo{modulo}')
+    if n - next != 0:
+        summands.append(n-next)
+
     # write your code here
     return summands
 

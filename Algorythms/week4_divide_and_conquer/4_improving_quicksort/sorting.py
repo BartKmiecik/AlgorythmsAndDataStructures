24a4c634a90f1cc8e68e7e3d160a993bb8422679
Bart <kmiecikjob@gmail.com>
2024-01-04 19:17:33 +0100

    Finally made it properly, stucked in some weird spot couldn't write it properly Quicksort alogyrthm

--- Algorythms/week4_divide_and_conquer/4_improving_quicksort/sorting.py
+++ Algorythms/week4_divide_and_conquer/4_improving_quicksort/sorting.py
@@ -2,33 +2,34 @@ from random import randint
 
 
 def partition3(array, left, right):
+    pivot = array[left]
+    # print(f'Left: {array[left]}')
     # print(array)
-    i = 0
-    j = 0
-    for n in range(left+1, right):
-        if array[left] > array[n]:
-            j += 1
-        else:
+    i = left + 1
+    for n in range(left+1,right+1):
+        if array[n] < pivot:
+            # print(f'N: {n} in array: {array[n]} less than: {pivot} i: {i} swapping: {array[i]}')
+            array[i], array[n] = array[n], array[i]
             i += 1
-            temp = j + i
-            array[temp], array[n] = array[n], array[temp]
 
-    array[left], array[i] = array[i], array[left]
-    left = i
+    array[left], array[i-1] = array[i-1], array[left]
 
-    return left, right
+    # print(array)
+    # print(f'i: {i} value {array[i]}')
+
+    return i -1
 
 
 def randomized_quick_sort(array, left, right):
     if left >= right:
         return
     k = randint(left, right)
-    # print(f'left: {array[left]}, k: {array[k]}')
+    # print(f'k: {array[k]}')
     array[left], array[k] = array[k], array[left]
-    # print(f'left: {array[left]}, k: {array[k]}')
-    m1, m2 = partition3(array, left, right)
+    m1 = partition3(array, left, right)
+
     randomized_quick_sort(array, left, m1 - 1)
-    randomized_quick_sort(array, m2 + 1, right)
+    randomized_quick_sort(array, m1 + 1, right)
 
 
 
@@ -36,9 +37,9 @@ if __name__ == '__main__':
     input_n = int(input())
     elements = list(map(int, input().split()))
     assert len(elements) == input_n
-    randomized_quick_sort(elements, 0, len(elements) - 1)
+    randomized_quick_sort(elements, 0, len(elements)-1)
     print(*elements)
 
-#
+
 # 5
 # 2 3 9 2 2

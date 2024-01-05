3547a1d98d8f16d936de36133cef0598414bdcd5
Bart <kmiecikjob@gmail.com>
2024-01-05 21:22:25 +0100

    Fixing bug

--- Algorythms/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
+++ Algorythms/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
@@ -52,15 +52,60 @@ def merge_sort(start, end):
 
 def points(start, end, points):
     var = len(points)
-    result = [0] * var
-    i = 0
+    temp = {}
+    for n in points:
+        temp[n] = 0
 
-    for n in range(len(start)):
-        if points[i] < start[n]:
-            i += 1
+    # print(f'Temp {temp}')
+    i = 0
+    j = 0
+    # print(points)
+    points.sort()
+    # print(points)
+
+    while j < len(points):
+        # print(f'i: {i}, j: {j}')
+        if points[j] < start[i]:
+            j += 1
         else:
-            if points[i] < end[n]:
-                result[i] += 1
+            if points[j] < end[i]:
+                # print(f'Should increase, {points[j]}, and value: {temp[points[j]]}')
+                temp[points[j]] += 1
+            i += 1
+        if i >= len(start):
+            break
+
+
+    # for n in range(len(points)):
+    #     print(f'n: {n}, points: {points[n]}, i: {i}, start: {start[i]}, end: {end[i]}')
+    #     if points[n] < start[i]:
+    #         n += 1
+    #     else:
+    #         if points[n] < end[i]:
+    #             print(f'Should increase, {points[n]}, and value: {temp[points[n]]}')
+    #             temp[points[n]] += 1
+    #             i += 1
+    #     if i >= len(start):
+    #         break
+
+
+    #
+    # for n in range(len(start)):
+    #     print(f'i: {i}, points: {points[i]}, n: {n}, start: {start[n]}')
+    #     print(points[i] < start[n])
+    #     print(f'i: {i}, points: {points[i]}, n: {n}, end: {end[n]}, point<end: {points[i] < end[n]}')
+    #     if points[i] < start[n]:
+    #         i += 1
+    #     else:
+    #         if points[i] < end[n]:
+    #             print('Should increase')
+    #             temp[points[i]] += 1
+
+    result = [0] * len(points)
+    i = 0
+    for _, value in temp.items():
+        result[i] = value
+        i += 1
 
     return result
 
@@ -75,5 +120,5 @@ if __name__ == '__main__':
     merge_sort(input_starts, input_ends)
     # print(f'Start: {input_starts}, end: {input_ends}')
 
-    output_count = points_cover_naive(input_starts, input_ends, input_points)
+    output_count = points(input_starts, input_ends, input_points)
     print(*output_count)

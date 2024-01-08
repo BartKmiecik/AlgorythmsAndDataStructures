7e73ae83efa281d8d5bb4442ebc08d19f419c56e
Bart <kmiecikjob@gmail.com>
2024-01-08 17:26:08 +0100

    Improving majority of elements

--- Algorythms/week4_divide_and_conquer/3_majority_element/majority_element.py
+++ Algorythms/week4_divide_and_conquer/3_majority_element/majority_element.py
@@ -5,12 +5,44 @@ def majority_element_naive(elements):
 
     return 0
 
-def majority_element(elements):
+def quick_sort(elements:list):
+    if len(elements) > 1:
+        mid = len(elements) // 2
+        left = elements[:mid]
+        right = elements[mid:]
+        quick_sort(left)
+        quick_sort(right)
 
-    pass
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                elements[k] = left[i]
+                i += 1
+            else:
+                elements[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            elements[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            elements[k] = right[j]
+            j += 1
+            k += 1
+
+def majority_element(elements):
+    quick_sort(elements)
+    mid = len(elements) // 2
+    if elements.count(elements[mid]) > len(elements) / 2:
+        return 1
+    return 0
 
 if __name__ == '__main__':
     input_n = int(input())
     input_elements = list(map(int, input().split()))
     assert len(input_elements) == input_n
-    print(majority_element_naive(input_elements))
+    print(majority_element(input_elements))

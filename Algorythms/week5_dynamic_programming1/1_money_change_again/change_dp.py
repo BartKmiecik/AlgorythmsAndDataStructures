573ad5104751c75ac77a894b22fd1d895076f0d8
Bart <kmiecikjob@gmail.com>
2024-01-14 14:57:18 +0100

    Fixing another edge case for change_dp

--- Algorythms/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Algorythms/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,11 +1,16 @@
 def change(money):
     # denomination: 1 3 4
+    if money == 0:
+        return 0
     if money == 2:
         return 2
     if money <= 4:
         return 1
     if money > 4:
-        money = (money // 4) + 1
+        mod4 = money % 4
+        money = money // 4
+        if mod4 > 0:
+            money += 1
 
     return int(money)
 

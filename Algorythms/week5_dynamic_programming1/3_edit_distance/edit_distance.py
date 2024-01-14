fedbe507663ab9734dfe61854b0bb1812f14552c
Bart <kmiecikjob@gmail.com>
2024-01-14 22:07:02 +0100

    Edit distance

--- Algorythms/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ Algorythms/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,6 +1,22 @@
-def edit_distance(first_string, second_string):
-    return 0
+def edit_distance(first_string, second_string, i, j, grid):
+    if not (i, j) in grid:
+        if i == 0:
+            grid[i, j] = j
+        elif j == 0:
+            grid[i, j] = i
+        else:
+            diff = 0 if first_string[i-1] == second_string[j-1] else 1
+            insertion = edit_distance(first_string, second_string, i, j -1, grid)
+            deletion = edit_distance(first_string, second_string, i - 1, j, grid)
+            missmatch = edit_distance(first_string, second_string, i -1, j -1, grid)
+            grid[i,j] = min(insertion, deletion, missmatch) + diff
 
+    return grid[i,j]
 
 if __name__ == "__main__":
-    print(edit_distance(input(), input()))
+    word1, word2 = input(), input()
+    grid = dict()
+    i = len(word1)
+    j = len(word2)
+    print(edit_distance(word1, word2, i, j, grid))
+    print(grid)

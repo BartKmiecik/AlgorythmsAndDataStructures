330291c43ba6904ba005ccc14394968c4dc8ffe5
Bart <kmiecikjob@gmail.com>
2024-01-14 22:41:02 +0100

    Work in progress

--- Algorythms/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ Algorythms/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,8 +1,21 @@
-def lcs2(first_sequence, second_sequence):
-    return 0
+def lcs2(first_sequence, second_sequence, grid):
+    if len(first_sequence) == 0 or len(second_sequence) == 0:
+        return 0
+    result = 0
+    for n in range(len(first_sequence)):
+        if first_sequence[n] in second_sequence:
+            result += 1
+            if not first_sequence[n] in grid:
+                grid[first_sequence[n]] = 1
+            else:
+                index = second_sequence.index(first_sequence[n])
+                next_res = lcs2(second_sequence[index:], first_sequence[n+1:], grid)
+                grid[first_sequence[n]] = next_res + result
+    return result
 
 
 if __name__ == '__main__':
+    grid = dict()
     n = int(input())
     a = list(map(int, input().split()))
     assert len(a) == n
@@ -11,4 +24,4 @@ if __name__ == '__main__':
     b = list(map(int, input().split()))
     assert len(b) == m
 
-    print(lcs2(a, b))
+    print(lcs2(a, b, grid))

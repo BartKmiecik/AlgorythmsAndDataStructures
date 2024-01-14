a1a4463b23c8204bfc4a748373ae5a1dee5db042
Bart <kmiecikjob@gmail.com>
2024-01-14 14:48:16 +0100

    Fixing edge case

--- Algorythms/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Algorythms/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,6 +1,10 @@
 def change(money):
     # denomination: 1 3 4
-    if money > 3:
+    if money == 2:
+        return 2
+    if money <= 4:
+        return 1
+    if money > 4:
         money = (money // 4) + 1
 
     return int(money)

78e8da281faef9c0a252e972ccb613d783b14ede
Bart <kmiecikjob@gmail.com>
2024-01-16 07:54:14 +0100

    Edit distance iteratively

--- Algorythms/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ Algorythms/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,4 +1,4 @@
-def edit_distance(first_string, second_string, i, j, grid):
+def edit_distance_recursive(first_string, second_string, i, j, grid):
     # print(f'I: {i}, J: {j}')
     if not (i, j) in grid:
         if i == 0:
@@ -7,22 +7,40 @@ def edit_distance(first_string, second_string, i, j, grid):
             grid[i, j] = i
         else:
             diff = 0 if first_string[i-1] == second_string[j-1] else 1
-            insertion = edit_distance(first_string, second_string, i, j -1, grid)
-            deletion = edit_distance(first_string, second_string, i - 1, j, grid)
-            missmatch = edit_distance(first_string, second_string, i -1, j -1, grid)
+            insertion = edit_distance_recursive(first_string, second_string, i, j -1, grid)
+            deletion = edit_distance_recursive(first_string, second_string, i - 1, j, grid)
+            missmatch = edit_distance_recursive(first_string, second_string, i -1, j -1, grid)
             grid[i,j] = min(insertion, deletion, missmatch) + diff
 
 
     return grid[i,j]
+
+def edit_distance_iterative(first, second):
+    arr2d = [[float("inf")] * (len(second) + 1) for _ in range(len(first)+1)]
+    for i in range(len(first)+1):
+        arr2d[i][0] = i
+    for j in range(len(second)+1):
+        arr2d[0][j] = j
+
+    for i in range(1,len(first)+1):
+        for j in range(1,len(second)+1):
+            # print(f'Comparing: {first[i-1]} and {second[j-1]}')
+            diff = 0 if first[i-1] == second[j-1] else 1
+            arr2d[i][j] = min(arr2d[i-1][j] + 1, arr2d[i][j-1] + 1, arr2d[i-1][j-1] + diff)
+
+    # print(arr2d)
+    return arr2d[len(first)][len(second)]
+
 
 if __name__ == "__main__":
     word1, word2 = input(), input()
     grid = dict()
     i = len(word1)
     j = len(word2)
-    result = edit_distance(word1, word2, i, j, grid)
-    diff = abs(i - j)
-    if result < diff:
-        result = diff
+    # result = edit_distance_recursive(word1, word2, i, j, grid)
+    result = edit_distance_iterative(first=word1, second=word2)
+    # diff = abs(i - j)
+    # if result < diff:
+    #     result = diff
     print(result)
     # print(grid)

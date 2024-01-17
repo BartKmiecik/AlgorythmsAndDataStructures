017f8c462a784e306b731ca463fcdd0d732b872b
Bart <kmiecikjob@gmail.com>
2024-01-17 07:57:16 +0100

    lcs2b improvment, but still on some edge case doesn't work properly

--- Algorythms/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2b.py
+++ Algorythms/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2b.py
@@ -1,22 +1,23 @@
-def lcs2(first_sequence, second_sequence, result):
+def lcs2(first_sequence, second_sequence):
     if len(first_sequence) == 0 or len(second_sequence) == 0:
         return 0
-    # result = 0
-    for n in range(len(first_sequence)):
-        index = -1
-        if first_sequence[n] in second_sequence:
-            result += 1
-            index = second_sequence.index(first_sequence[n])
-        if n != len(first_sequence) - 1:
-            lcs2(first_sequence[n+1:], second_sequence[index+1:], result)
-
+    result = 0
+    index = -1
+    if first_sequence[0] in second_sequence:
+        result += 1
+        index = second_sequence.index(first_sequence[0])
+    result += lcs2(first_sequence[1:], second_sequence[index + 1:])
     return result
 
 
-def lcs2_full(first_sequence, second_sequence, grid):
-    lcs2(first_sequence, second_sequence, grid)
-    grid = sorted(grid.items(), reverse=True, key=lambda v: v[1])
-    return grid[0][1]
+def lcs2_full(first_sequence, second_sequence):
+    total_result = 0
+    for n in range(len(first_sequence)):
+        result = lcs2(first_sequence[n:], second_sequence)
+        if result > total_result:
+            total_result = result
+
+    return total_result
 
 
 if __name__ == '__main__':
@@ -29,5 +30,5 @@ if __name__ == '__main__':
     b = list(map(int, input().split()))
     assert len(b) == m
 
-    print(lcs2(a, b, 0))
+    print(lcs2_full(a, b))
     # print(GR)

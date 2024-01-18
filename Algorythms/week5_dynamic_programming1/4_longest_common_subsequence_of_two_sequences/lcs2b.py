c0349b36884f7c25445731f31931ac4b7f519b08
Bart <kmiecikjob@gmail.com>
2024-01-18 08:04:24 +0100

    Messing around with lcs2

--- Algorythms/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2b.py
+++ Algorythms/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2b.py
@@ -1,20 +1,28 @@
-def lcs2(first_sequence, second_sequence):
+def lcs2(first_sequence, second_sequence,n, m, memo = {}):
     if len(first_sequence) == 0 or len(second_sequence) == 0:
         return 0
-    result,res1 = 0, 0
-    index = -1
+    # if first_sequence[0] == -6:
+    #     print('asd')
+    index = 0
     if first_sequence[0] in second_sequence:
         index = second_sequence.index(first_sequence[0])
-        res1 = lcs2(first_sequence[1:], second_sequence[index + 1:]) + 1
-    res2 = lcs2(first_sequence[1:], second_sequence)
+    key = f'{n - len(first_sequence), index + m - len(second_sequence) }'
+    if key in memo:
+        return memo[key]
+    result,res1 = 0, 0
+    if first_sequence[0] in second_sequence:
+        res1 = lcs2(first_sequence[1:], second_sequence[index + 1:], n, m) + 1
+    res2 = lcs2(first_sequence[1:], second_sequence, n, m)
     result += max(res1, res2)
+    memo[key] = result
+
     return result
 
 
-def lcs2_full(first_sequence, second_sequence):
+def lcs2_full(first_sequence, second_sequence,n, m):
     total_result = 0
-    for n in range(len(first_sequence)):
-        result = lcs2(first_sequence[n:], second_sequence)
+    for i in range(len(first_sequence)):
+        result = lcs2(first_sequence[i:], second_sequence,n, m)
         if result > total_result:
             total_result = result
 
@@ -31,5 +39,5 @@ if __name__ == '__main__':
     b = list(map(int, input().split()))
     assert len(b) == m
 
-    print(lcs2_full(a, b))
+    print(lcs2_full(a, b, n, m))
     # print(GR)
